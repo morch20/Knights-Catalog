54e18f0f022475ea88e5e627be94b0589cb96f1c
Joseph <josephmt1176@gmail.com>
2022-07-13 01:04:41 -0400

    server - index.js - starting using mongodb instead of mongoose and added '/programSearch' route for testing

--- server/index.js
+++ server/index.js
@@ -1,14 +1,41 @@
 const express = require('express');
-const mongoose = require('mongoose');
 require('dotenv').config();
+const {MongoClient} = require('mongodb');
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-mongoose.connect("lola", { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(() => app.listen(PORT, () => {console.log(`Server running on port ${PORT}`)}))
+let client;
+
+MongoClient.connect(process.env.CONNECTION_URI)
+    .then(c => {
+        client = c;
+        app.listen(PORT, () => console.log(`Server listening to port ${PORT}`));
+    })
     .catch(err => console.log(err))
 
-app.get('/', (res, req) => {
-    req.send('api working!')
+app.get('/', (req, res) => {
+    res.send('api working!')
+})
+
+app.get('/programSearch', (req, res) => {
+
+    const db = client.db("programSearch");
+
+    const page = req.query.p || 0;
+    const documentsPerPage = 20;
+
+  let documents = []
+
+  db.collection('programSearch')
+    .find()
+    .skip(page * documentsPerPage)
+    .limit(documentsPerPage)
+    .forEach(document => documents.push(document))
+    .then(() => {
+      res.status(200).json(documents)
+    })
+    .catch(() => {
+      res.status(500).json({error: 'Could not fetch the documents'})
+    })
 })
