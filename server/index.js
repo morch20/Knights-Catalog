69d2b1a815ee5ebd38b39cb407f1a168feb2a813
Joseph <josephmt1176@gmail.com>
2022-07-13 23:58:48 -0400

    server - index.js - added searchRoute

--- server/index.js
+++ server/index.js
@@ -1,41 +1,16 @@
 const express = require('express');
 require('dotenv').config();
-const {MongoClient} = require('mongodb');
+
+const searchRoute = require('./routes/search.js');
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-let client;
 
-MongoClient.connect(process.env.CONNECTION_URI)
-    .then(c => {
-        client = c;
-        app.listen(PORT, () => console.log(`Server listening to port ${PORT}`));
-    })
-    .catch(err => console.log(err))
 
-app.get('/', (req, res) => {
-    res.send('api working!')
-})
+app.listen(PORT, () => console.log(`Server listening to port ${PORT}`));
 
-app.get('/programSearch', (req, res) => {
-
-    const db = client.db("programSearch");
-
-    const page = req.query.p || 0;
-    const documentsPerPage = 20;
-
-  let documents = []
-
-  db.collection('programSearch')
-    .find()
-    .skip(page * documentsPerPage)
-    .limit(documentsPerPage)
-    .forEach(document => documents.push(document))
-    .then(() => {
-      res.status(200).json(documents)
-    })
-    .catch(() => {
-      res.status(500).json({error: 'Could not fetch the documents'})
-    })
-})
+app.use('/search', searchRoute);
+// app.use('/undergraduate', searchRoute);
+// app.use('/graduate', searchRoute);
+// app.use('/ratings', searchRoute);
