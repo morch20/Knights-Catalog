dd868b49a7a8b9cbc6e6e3ccb2867e800ceb59a8
Joseph <josephmt1176@gmail.com>
2022-07-15 20:13:28 -0400

    server - index.js added undergraduate route

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ const cors = require('cors');
 require('dotenv').config();
 
 const searchRoute = require('./routes/search.js');
+const undergraduateRoute = require('./routes/undergraduate.js');
 
 const app = express();
 app.use(cors());
@@ -13,6 +14,6 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server listening to port ${PORT}`));
 
 app.use('/search', searchRoute);
-// app.use('/undergraduate', searchRoute);
+app.use('/undergraduate', undergraduateRoute);
 // app.use('/graduate', searchRoute);
 // app.use('/ratings', searchRoute);
