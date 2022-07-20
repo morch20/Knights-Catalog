3412aa26e27846aeb9cba85ea6cedf13705a6461
Joseph <josephmt1176@gmail.com>
2022-07-19 20:59:35 -0400

    client - Added Home to pages Started working on header

--- client/src/Pages/Home.jsx
+++ client/src/Pages/Home.jsx
@@ -1,8 +1,36 @@
 import React from 'react'
+import { ReactComponent as Logo } from '../assets/header.svg'
 
 const Home = () => {
   return (
-    <div >Home</div>
+    <>
+      <header className='header__gradient__light flex my-8 sm:my-12 mx-4 sm:mx-[3rem] md:mx-[4.5rem] md:justify-between'>
+
+        <div className=' md:pr-10 '>
+
+          <h1 className=' font-semibold text-[2.5rem] flex flex-col md:flex-row'>
+            <span>
+              Step Up Your
+            </span> 
+            <span className='text-[color:var(--yellow)] pl-4'>
+              Education
+            </span>
+          </h1>
+
+          <p className=' font-normal text-base text-[color:var(--secondary__light)] mt-8'>
+            Stairs brings together your team's working docs and important discussions. 
+            Move projects faster, work more asynchronously, and feel connected
+          </p>
+
+        </div >
+
+        <div className=' px-5 hidden md:block'>
+          <Logo/>
+        </div>
+
+      </header>
+
+    </>
   )
 }
 
