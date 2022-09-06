9804c91f1f570d0fe29d269f60cad7ed48850cea
Joseph <josephmt1176@gmail.com>
2022-09-05 21:28:46 -0400

    client - home/ added background for HeaderImage fixed web responsiveness

--- client/src/App.js
+++ client/src/App.js
@@ -5,17 +5,19 @@ import { Home, Graduate, Undergraduate, Explore, Ratings } from './Pages/index.j
 
 const App = () => {
   return (
-    <BrowserRouter>
-      <Navbar/>
-      <div className='h-[100px] xl:h-[120px]'></div>
-      <Routes>
-        <Route path="/" element={<Home></Home>}/>
-        <Route path="/graduate" element={<Graduate></Graduate>}/>
-        <Route path="/undergraduate" element={<Undergraduate></Undergraduate>}/>
-        <Route path="/explore" element={<Explore></Explore>}/>
-        <Route path="/ratings" element={<Ratings></Ratings>}/>
-      </Routes>
-    </BrowserRouter>
+      <BrowserRouter>
+        <Navbar/>
+        <div className='h-[100px] xl:h-[120px]'></div>
+        <div className='mx-8 xsm:mx-10 sm:mx-12 lg:mx-16'>
+          <Routes>
+            <Route path="/" element={<Home></Home>}/>
+            <Route path="/graduate" element={<Graduate></Graduate>}/>
+            <Route path="/undergraduate" element={<Undergraduate></Undergraduate>}/>
+            <Route path="/explore" element={<Explore></Explore>}/>
+            <Route path="/ratings" element={<Ratings></Ratings>}/>
+          </Routes>
+        </div>
+      </BrowserRouter>
   )
 }
 
--- client/src/Pages/home/Home.jsx
+++ client/src/Pages/home/Home.jsx
@@ -5,11 +5,11 @@ import arrow from '../../assets/57.svg';
 const Home = () => {
   return (
     <>
-      <header className='header__gradient__light flex lg:py-0 2xl:pt-20 pl-4 md:pl-[4.5rem] xsm:justify-center md:justify-between'>
+      <header className=' flex flex-col-reverse md:flex-row lg:py-0 2xl:pt-20 xsm:justify-center md:justify-between min-w-full'>
 
-        <div className='lg:pl-10'>
+        <div className='flex flex-col items-center text-center md:items-start md:text-left mt-8 xsm:mt-14 xl:mt-1'>
 
-          <h1 className=' font-medium text-[2.5rem] lg:text-[2.8rem] !m-0 pl-4 flex flex-col md:pl-0 2xl:flex-row w-fit slide-right'>
+          <h1 className=' leading-tight font-semibold text-[2.5rem] lg:text-[3.5rem] !m-0 flex flex-col w-fit slide-right'>
             <span>
               Step Up Your
             </span> 
@@ -18,23 +18,21 @@ const Home = () => {
             </span>
           </h1>
 
-          <p className=' max-w-[30rem] 2xl:max-w-[45rem] px-5 md:px-0 md:pr-20 font-normal text-base text-[color:var(--text-secondary-light)] mt-10 xsm:mt-14 md:mt-8'>
-            Stairs brings together your team's working docs and important discussions. 
-            Move projects faster, work more asynchronously, and feel connected
+          <p className=' max-w-[25rem] 2xl:max-w-[45rem] lg:text-lg xl:text-xl font-normal text-base text-[color:var(--text-secondary-light)] mt-10 lg:mt-14 xsm:mt-14 md:mt-8'>
+            Build your brand's recognition and get detailed insights on how your
+            links are performing .
           </p>
 
-          <div className='flex slide-up'>
-            <button className=' ml-5 sm:ml-0 my-10 lg:my-14 mr-4 rounded-[14px] bg-[color:var(--yellow)] font-bold text-white w-40 h-11 scale-up-center-active'>
+          <div className='flex slide-up relative'>
+            <button className=' my-8 rounded-[14px] bg-[color:var(--yellow)] font-bold text-white w-40 h-11 lg:h-12 lg:text-lg hover:bg-yellow-300'>
               Start Here!
             </button>
-              <img src={arrow} className='w-20 h-20 lg:mt-4 xl:mt-6'></img>
+              <img src={arrow} className='w-20 h-20 absolute right-[-5rem] md:right-[-6rem]'></img>
           </div>
 
         </div >
 
-        <div className=' lg:px-5 hidden sm:block md:w-[50%] md:h-[50%] 2xl:w-[40%] 2xl:h-[40%] pt-10 md:mr-16 lg:mr-20'>
-          <HeaderImage></HeaderImage>
-        </div>
+        <HeaderImage/>
 
       </header>
 
