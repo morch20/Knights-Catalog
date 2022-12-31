300092de8d0fe5d460e34d2ab252f90adfbfe966
Joseph <josephmt1176@gmail.com>
2022-12-31 12:22:54 -0500

    client - added dependency array to useOutsideClick

--- client/src/hooks/useOutsideClick.js
+++ client/src/hooks/useOutsideClick.js
@@ -1,12 +1,28 @@
 import { useRef, useEffect } from "react";
 
-const useOutsideClick = (callback) =>{
+const useOutsideClick = (callback, dependencies) =>{
 
   const ref = useRef();
 
+  const validateDependencies = (event) => {
+
+    if(dependencies === undefined) return true;
+
+    let flag = true;
+    
+    for (let i = 0; i < dependencies.length; i++) {
+      if(dependencies[i].current.contains(event.target)){
+        flag = false;
+        break;
+      }  
+    }
+
+    return flag;
+  }
+
   useEffect(() => {
     const handleClick = (event) => {
-      if (ref.current && !ref.current.contains(event.target)) {
+      if (ref.current && !ref.current.contains(event.target) && validateDependencies(event)) {
         callback();
       }
     };
