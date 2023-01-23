a81ba3a2e292177713b91f454421c307f5210acc
Joseph <josephmt1176@gmail.com>
2023-01-22 19:08:14 -0500

    client - utils - changed colors of colleges

--- client/src/utils/constants.js
+++ client/src/utils/constants.js
@@ -3,17 +3,17 @@ export const regexCodeWithSpace = /[A-Z]{3}\s[0-9]{4}/;
 export const regexCode = /[A-Z]{3}[0-9]{4}/;
 
 export const collegesColors = {
-    "College of Arts and Humanities": "bg-pink-300",
+    "College of Arts and Humanities": "bg-pink-500",
     "College of Business Administration": "bg-slate-800",
-    "College of Community Innovation and Education": "bg-yellow-200",
+    "College of Community Innovation and Education": "bg-yellow-500",
     "College of Engineering and Computer Science": "bg-gray-600",
-    "College of Sciences": "bg-lime-300",
-    "College of Health Professions and Sciences": "bg-green-300",
+    "College of Sciences": "bg-lime-500",
+    "College of Health Professions and Sciences": "bg-green-400",
     "College of Nursing": "bg-green-600",
     "College of Medicine": "bg-green-900",
     "College of Optics and Photonics": "bg-violet-600",
     "College of Undergraduate Studies": "bg-stone-500",
-    "Rosen College of Hospitality Management": "bg-teal-300",
+    "Rosen College of Hospitality Management": "bg-teal-500",
     "Burnett Honors College": "bg-amber-800"
 
 };
