2aac81b7ce042b7cb45302add6df36e343424772
Joseph <josephmt1176@gmail.com>
2023-05-20 17:11:34 -0400

    client - functions - created getTagsHighlights

---
+++ client/src/utils/functions.js
@@ -0,0 +1,33 @@
+import { regexCodeWithSpace, regexCode } from './constants';
+
+export function getTagsHighlights(text, highlight){
+
+    const arr = text?.split("\n");
+
+    arr?.forEach(element => {
+
+        if(element){
+            
+            if(element.substring(0,2) === "• "){
+                element = element.replace("• ", "");
+            }
+
+            if(element.search(regexCode) === 0){
+                if(highlight.current.hasOwnProperty(element.substring(0,8)) ){
+                    highlight.current[element.substring(0,8)] = true;
+                }
+                else{
+                    highlight.current[element.substring(0,8)] = false;
+                }
+            }
+            else if(element.search(regexCodeWithSpace) === 0){
+                if(highlight.current.hasOwnProperty(element.substring(0,9)) ){
+                    highlight.current[element.substring(0,9)] = true;
+                }
+                else{
+                    highlight.current[element.substring(0,9)] = false;
+                }
+            }
+        }
+    })
+}
